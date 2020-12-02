6f09c40350436380b8c9945a6de5ab2f2326c9f9
Matttokin <55289178+Matttokin@users.noreply.github.com>
2020-12-02 11:02:38 +0300

    add database

---
+++ WSR_NPI/Controllers/Web/FinishOrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WSR_NPI.DataBase;
+
+namespace WSR_NPI.Controllers.Web
+{
+    public class FinishOrderController : ApiController
+    {
+        public string Post(string token)
+        {
+            Context db = new Context();
+            var user = db.Users.FirstOrDefault(x => x.Token.Equals(token));
+
+            if (user != null)
+            {
+                var courier = db.Сouriers.FirstOrDefault(x => x.User.Id == user.Id);
+                courier.Status = "Свободен";
+                var order = db.Orders.FirstOrDefault(x => x.Id == courier.User.Id);
+                order.Status = "Доставлен";
+                courier.OrderId = null;
+                db.SaveChanges();
+
+                return "Успешно";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WSR_NPI/Controllers/Web/GetOrderByIdController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WSR_NPI.DataBase;
+using WSR_NPI.DataBase.Models;
+
+namespace WSR_NPI.Controllers.Web
+{
+    public class GetOrderByIdController : ApiController
+    {
+
+        public IEnumerable<Nomenclature> Get(int idOrder)
+        {
+            Context db = new Context();
+
+            var orderNoms = db.OrderNoms.Where(x => x.OrderId == idOrder).Include(x => x.Nomenclature).Select(x => x.Nomenclature).ToList();
+
+            var order = db.Orders.Include(x => x.OrderNoms).FirstOrDefault(x => x.Id == idOrder);
+            return orderNoms;
+        }
+    }
+}
